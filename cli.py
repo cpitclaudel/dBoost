f24a7cf31c74b105933b4505682bdce1ee5f4652
Clément Pit--Claudel <clement.pit-claudel@polytechnique.edu>
2014-11-21 03:55:09 -0500

    Basic command line interface

---
+++ cli.py
@@ -0,0 +1,28 @@
+import argparse
+from models import gaussian, discrete
+
+REGISTERED_MODELS = (gaussian.Simple, gaussian.Mixture, discrete.Histogram)
+
+def get_base_parser():
+    base_parser = argparse.ArgumentParser(add_help = False)
+    base_parser.add_argument("-F", "--field-separator", nargs = 1, dest = "fs",
+                             action = "store", default = "\t")
+
+    base_parser.add_argument("-v", "--verbose", dest = "verbosity",
+                             action = "store_const", const = 1, default = 0)
+
+    base_parser.add_argument("-vv", "--debug", dest = "verbosity",
+                             action = "store_const", const = 2)
+    
+    for model in REGISTERED_MODELS:
+        model.register(base_parser)
+
+    return base_parser
+
+def load_models(namespace):
+    models = []
+    for model in REGISTERED_MODELS:
+        params = getattr(namespace, model.ID)
+        if params != None:
+            models.append(model.from_parse(params))
+    return models
