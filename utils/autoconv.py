7c5932fa5da0a0601bea49f43420a2ea2d947cfe
Clément Pit--Claudel <clement.pitclaudel@live.com>
2014-12-13 18:49:40 -0500

    Clean up autoconv.py

--- utils/autoconv.py
+++ utils/autoconv.py
@@ -1,14 +1,14 @@
+def autoconv_step(field, converters, offset):
+    if offset >= len(converters):
+        return value
+
+    try:
+        field = converters[offset](field)
+    except Exception as e:
+        field = autoconv_step(field, converters, offset + 1)
+
+    return field
+
 def autoconv(field, floats_only = False):
-    def autoconv_step(value, converters, offset):
-        if offset > len(converters):
-            return value
-
-        try:
-            field = converters[offset](field)
-        except:
-            field = autoconv_step(value, converters, offset + 1)
-
-        return field
-
     converters = [float] if floats_only else [int, float]
     return autoconv_step(field, converters, 0)
