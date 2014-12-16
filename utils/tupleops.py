b8cd20661205c23063be5b885a85e7a37e415474
Clément Pit--Claudel <clement.pitclaudel@live.com>
2014-12-16 17:05:04 -0500

    Add a few extra tuple functions

--- utils/tupleops.py
+++ utils/tupleops.py
@@ -10,13 +10,19 @@ def zeroif(S, X):
     return S if S != None else tuple(tuple(0 for _ in x) for x in X)
 
 def root(X):
-    return tuple(tuple(sqrt(xi) for xi in x) for x in X)
+    return deepmap(sqrt, X) #TODO remove
 
 def deepmap(f, X):
     return tuple(tuple(f(xi) for xi in x) for x in X)
 
+def filter(f, X):
+    return tuple(tuple((xi if (xi != None and f(xi)) else None) for xi in x) for x in X)    
+
 def merge(S, X, f, phi):
     return tuple(tuple(phi(si, f(xi)) for si, xi in zip(s, x)) for s, x in zip(S, X))
+
+def number(X):
+    return tuple(tuple((i, j) for j, _ in enumerate(x)) for i, x in enumerate(X))
 
 def id(x):
     return x
@@ -29,7 +35,7 @@ def not_null(x):
 
 def keep_if(a, b):
     return a if b else None
-    
+
 def plus(a, b):
     return a + b if b != None else a
 
@@ -42,6 +48,11 @@ def mul(a, b):
 def div0(a, b):
     return a / b if a != None and b != 0 else 0
 
+def incrkey(a, b):
+    if a != None:
+        a[b] += 1
+    return a
+        
 def tuplify(a, b):
     return (a, b)
 
