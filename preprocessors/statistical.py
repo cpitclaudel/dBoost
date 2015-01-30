50ea9b038e0be4f140aced644c8cac64b0c4182a
Clément Pit--Claudel <clement.pitclaudel@live.com>
2015-01-30 17:59:21 -0500

    Fix a bug in statistical.py
    Correlations involving the first field were never reported, due to
    6038ad21d35bdf9dbd39ca98877844a5bf6ce452 failing to revert
    c1ffc7dcf6a9ed09e4708d5fb89882c01ff61034.

--- preprocessors/statistical.py
+++ preprocessors/statistical.py
@@ -4,7 +4,7 @@ from utils.autoconv import autoconv
 import math,itertools
 
 # Preprocessor that collects dataset statistics over columns for use by other models.
-#   It also detects correlations between columns using the Pearson R coefficient. 
+#   It also detects correlations between columns using the Pearson R coefficient.
 #   If the absolute value of the R value is greater than epsilon, the coordinates of
 #   the two columns are added as pairs to hints in the format
 #   ( (x-index, x-subcolumn-index), (y-index, y-subcolumn-index))
@@ -15,7 +15,7 @@ import math,itertools
 #   minm -- minimum value in the column
 #   ttl -- sum of values in the column
 #   var -- variance
-# 
+#
 
 class Pearson:
     ID = "statistical"
@@ -25,7 +25,7 @@ class Pearson:
         self.reset()
 
     def reset(self):
-      self.hints = [] 
+      self.hints = []
       self.sum = []
       self.avg = []
       self.var = []
@@ -38,24 +38,22 @@ class Pearson:
     def register(parser):
         parser.add_argument("--" + Pearson.ID, nargs = 1, metavar = "epsilon",
                             help = "Use a statistical model preprocessor, reporting correlated values " +
-                            "with a pearson r value greater than epsilon.") 
- 
+                            "with a pearson r value greater than epsilon.")
+
     @staticmethod
     def from_parse(params):
         return Pearson(*map(float, params))
-        
+
     def fit(self, Xs):
         S,S2,C,SXY = None, None, None,None
 
         for (nb, X_) in enumerate(Xs):
-            # discard first tuple, since this is empty hints tuple
-            X_ = X_[1:len(X_)]
             X_ = filter_abc(X_, numbers.Number)
             S, S2, C= zeroif(S, X_), zeroif(S2, X_), zeroif(C, X_)
             S = merge(S, X_, id, plus)
             S2 = merge(S2, X_, sqr, plus)
             C = merge(C, X_, not_null, plus)
-            XY_ = () 
+            XY_ = ()
             idx = -1
             for ((X,Y),(nx,ny)) in zip(itertools.combinations(X_,2),itertools.combinations(range(len(X_)),2)):
               for ((x,y),(nnx,nny)) in zip(itertools.product(zip(*[X]),zip(*[Y])),itertools.product(range(len(X)),range(len(Y)))):
@@ -69,7 +67,7 @@ class Pearson:
         AVGSQX = merge(S2,C,id,div0)
         VARX = merge(AVGSQX,AVGX,sqr,minus)
 
-        idx = -1 
+        idx = -1
         PR = ()
         VARXY = ()
 
@@ -92,10 +90,9 @@ class Pearson:
               self.hints.append(((nx,nnx),(ny,nny)))
 
         self.cnt = C
-        self.sum = S 
-        self.sum2 = S2 
-        self.avg = AVGX 
-        self.avg2 = AVGSQX 
-        self.var = VARX 
+        self.sum = S
+        self.sum2 = S2
+        self.avg = AVGX
+        self.avg2 = AVGSQX
+        self.var = VARX
         self.pearson = PR
-
