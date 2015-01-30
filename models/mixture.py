295cbce8a50218b47b605a5d4bb1d61a920a8bc9
ZeldaMariet <zmariet@mit.edu>
2015-01-29 20:37:55 -0500

    Changed outlier detection in Mixture Model

--- models/mixture.py
+++ models/mixture.py
@@ -2,15 +2,15 @@ import numbers
 from utils.tupleops import *
 from utils.autoconv import autoconv
 from sklearn import mixture
-from numpy import array, percentile
+from matplotlib import pyplot
+from math import erf
 
 class Mixture:
     ID = "mixture"
     
     def __init__(self, n_components, cutoff):
-        #FIXME: currently unused, using self.cutoffs instead
-        self.cutoff = cutoff
         self.n_components = n_components
+        self.cutoff = cutoff
         
     def reset(self):
         self.gmms    = None
@@ -19,18 +19,27 @@ class Mixture:
         self.keep    = None
         
     @staticmethod
-    def register(parser): #TODO (Z): fix the doc.
+    def register(parser): 
         parser.add_argument("--" + Mixture.ID, nargs = 2, metavar = ("n_subpops", "threshold"),
                             help = "Use a gaussian mixture model, reporting values whose probability is " +
-                            "below the threshold percentile, as predicted by a model of the data comprised of n_subpops "+
-                            "gaussians. Suggested values: 2, TODO.")
+                            "below the threshold, as predicted by a model of the data comprised of n_subpops "+
+                            "gaussians. Suggested values: 2, 0.3.")
         
     @staticmethod
     def from_parse(params):
         return Mixture(*map(autoconv, params))
 
+    def mahanoblis(self, x, gmm, component):
+        mean = gmm.means_[component]
+        covar = gmm.covars_[component]
+        u = x - mean
+        v = u.transpose()
+
+        return sqrt(v.dot(((1 / covar) * u).transpose()))
+
     def fit(self, Xs):
         self.gmms = []
+        self.evt = []
         self.cutoffs = []
 
         correlations = []
@@ -42,23 +51,25 @@ class Mixture:
             gmm = mixture.GMM(n_components = self.n_components)
             gmm.fit(to_fit)
             self.gmms.append(gmm)
+            
+            lp, resp = self.gmms[c].score_samples(to_fit)
+            lp_model = mixture.GMM(n_components = 2)
 
-            lp, _ = self.gmms[c].score_samples(to_fit)
-            #from matplotlib import pyplot
-            #pyplot.hist(lp)
-            #pyplot.show()
-            #FIXME: change cutoff value
-            self.cutoffs.append(percentile(array(lp), self.cutoff))
+            # ps = [self.test_one(x, c) for x in to_fit]
+            # pyplot.hist(ps, bins = 30)
+            # pyplot.show()
 
-    def test_one(self, xi, gmm, cutoff):
-        return gmm.score([xi]) <= cutoff
-
+    def test_one(self, xi, gmm_pos):
+        gmm = self.gmms[gmm_pos]
+        hx = zip(gmm.weights_, [1-erf(self.mahanoblis(xi, gmm, i))/sqrt(2) for i in range(self.n_components)])
+        return sum([w*h for (w,h) in hx])
+	
     def find_discrepancies(self, X, index):
         correlations = X[0]
         discrepancies = []
         
-        for id, (correlation, gmm, cutoff) in enumerate(zip(correlations, self.gmms, self.cutoffs)):
-            if not self.test_one(correlation, gmm, cutoff):
+        for id, (correlation, gmm_pos, cutoff) in enumerate(zip(correlations, range(len(self.gmms)), range(len(self.gmms)))):
+            if self.test_one(correlation, gmm_pos) < self.cutoff:
                 discrepancies.append(((0, id),))
 
         return discrepancies
