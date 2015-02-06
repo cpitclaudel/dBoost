c0110e99d79105444bde686fda9916ba0a5ff279
Rachael Harding <rhardin@mit.edu>
2015-02-06 10:29:35 -0500

    Added sensor plotting

---
+++ graphics/sensor-plots.py
@@ -0,0 +1,26 @@
+from utils import filename
+from utils.plots_helper import sensors 
+import matplotlib
+from matplotlib import pyplot
+from matplotlib.backends.backend_pdf import PdfPages
+
+make,fname = filename("sensor-plots.pdf")
+dfile = "../datasets/real/intel/sensors-1000-dirty.txt"
+
+# e, p, t, y, x
+args = [
+    (0,1,0.005,0,1),
+    (0,1,0.005,0,3),
+    (0.7,1,0.05,0,1),
+    (0.7,1,0.05,0,3),
+    (0.7,2,0.25,0,1),
+    (0.7,2,0.25,0,3)]
+
+pdf = PdfPages(fname)
+for (e,p,t,y,x) in args:
+    title = "Outliers in Sensor Data\n"+str(p)+" Gaussians,$\\theta$=" + str(t)
+    ofile = "../results/sensors_dirty_stat" + str(e) + "_mix" + str(p) + "_" + str(t) + ".out"
+    sensors(title,x,y,dfile,ofile)
+    pyplot.savefig(pdf, format = 'pdf', transparent = True)
+    pyplot.clf()
+pdf.close()
