0ba2509c77814646aa235093b5d109e1bd91a69a
Clément Pit--Claudel <clement.pitclaudel@live.com>
2015-02-08 12:23:54 -0500

    graphics: Use green and red

--- graphics/utils/plots_helper.py
+++ graphics/utils/plots_helper.py
@@ -1,10 +1,21 @@
 import csv
 import matplotlib as mpl
-from matplotlib import pyplot, mlab
+from matplotlib import pyplot
 from mpl_toolkits.mplot3d import Axes3D
 from utils import TANGO
 
-sensors_schema = ["Temperature (C)",r"Humidity ($\%$)","Light","Voltage (V)"]
+mpl.rcParams['text.latex.preamble'] = [r"\usepackage{siunitx}"]
+
+LOF_SCALE, BASE_SIZE = 5, 10
+MARKER_ALPHA, EDGE_WIDTH, = 1, 0.1
+
+def get_marker_params(color, **kwargs):
+    return dict(marker='o', color=TANGO[color][0], edgecolor=TANGO[color][2],
+                alpha=MARKER_ALPHA, linewidth=EDGE_WIDTH, **kwargs)
+INLIER = get_marker_params('green', s=BASE_SIZE)
+OUTLIER = get_marker_params('red')
+
+sensors_schema = [r"Temperature (\si{\degreeCelsius})",r"Humidity (\si{\percent})",r"Light (TODO: Unit)",r"Voltage (\si{\volt})"]
 
 def get_sensor_data(fname):
     d = [[],[],[],[]]
@@ -35,8 +46,8 @@ def sensors(title,x,y,dfile,ofile):
         if l in outliers:
             o[y].append(d[y][l])
             o[x].append(d[x][l])
-    ax.scatter(d[x],d[y],color=TANGO["grey"][1],marker="o",alpha = 1)
-    ax.scatter(o[x],o[y],color=TANGO["black"][2],marker="o")
+    ax.scatter(d[x],d[y],**INLIER)
+    ax.scatter(o[x],o[y], s=BASE_SIZE, **OUTLIER)
 
 def lof(title,dfile,ofile):
     d = get_sensor_data(dfile)
@@ -44,11 +55,10 @@ def lof(title,dfile,ofile):
     ax.set_title(title)
     ax.set_xlabel(sensors_schema[1])
     ax.set_ylabel(sensors_schema[0])
-    ax.scatter(d[1],d[0],marker='o',color=TANGO["grey"][1],alpha = 1)
+    ax.scatter(d[1],d[0],**INLIER)
     with open(ofile,'r') as f:
         rdr = csv.reader(f,delimiter=' ')
         for l in rdr:
             l = [float(x) for x in l]
             if l[0] < 1.5: continue
-            ax.scatter(l[2],l[1],marker='o',color=TANGO["black"][2],s=(l[0]*10))
-
+            ax.scatter(l[2],l[1],s=(l[0]*LOF_SCALE),**OUTLIER)
