2469e9be4d42f46dc59e6ef3435c7c04e77ee06b
Clément Pit--Claudel <clement.pitclaudel@live.com>
2015-02-08 23:33:48 -0500

    graphics/paper: Simple Gaussian 3D plots

--- graphics/3d-plots.pdf.py
+++ graphics/3d-plots.pdf.py
@@ -1,23 +1,28 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
 from utils import filename, save2pdf
 from utils.plots_helper import sensors3D
 from matplotlib.backends.backend_pdf import PdfPages
 from matplotlib import pyplot
 
-batch, fname = filename("sensor-mix-plots.pdf")
+batch, fname = filename("3d-plots.pdf")
 dfile = "../datasets/real/intel/sensors-1000-dirty.txt"
 
-# e, p, t
-args = [(0,1,0.005),  # from sensor-plots (p3)
-        (0.7,1,0.1),  # from sensor-plots (p1 and p2)
-        (0.7,2,0.05)] # from sensor-mix-plots (p1 and p2)
+MIX_OUTFILE = "../results/sensors_dirty_stat{}_mix{}_{}.out"
+GAUSS_OUTFILE = "../results/sensors_dirty_stat{}_gaus{}.out"
+
+
+sources = [(GAUSS_OUTFILE, 1, 1.5),     # from gauss-plots
+           (MIX_OUTFILE, 0, 1, 0.005),  # from sensor-plots (p3)
+           (MIX_OUTFILE, 0.7, 1, 0.1),  # from sensor-plots (p1 and p2)
+           (MIX_OUTFILE, 0.7, 2, 0.05)] # from sensor-mix-plots (p1 and p2)
 
 if batch:
     pdf = PdfPages(fname)
 
-for e, p, t in args:
-    ofile = "../results/sensors_dirty_stat" + str(e) + "_mix" + str(p) + "_" + str(t) + ".out"
+for out_template, *args in sources:
+    ofile = out_template.format(*args)
     sensors3D(dfile,ofile)
+
     if batch:
         pyplot.savefig(pdf, pad_inches=1, format = 'pdf')
     else:
