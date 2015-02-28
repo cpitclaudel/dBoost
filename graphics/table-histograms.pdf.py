f958e43c3b802a59a2ed598f31493897798f1578
Clément Pit--Claudel <clement.pitclaudel@live.com>
2015-02-28 16:56:10 -0500

    graphics: Final touches to the plot

--- graphics/table-histograms.pdf.py
+++ graphics/table-histograms.pdf.py
@@ -6,9 +6,6 @@ from matplotlib import pyplot
 FILL  = [TANGO["red"][0], TANGO["green"][1]]
 EDGES = [TANGO["red"][1], TANGO["green"][2]]
 
-def format_label(l):
-    return r"\makebox[2cm][r]{{{}}}".format(l)
-
 def make_hist(ys, labels):
     W = 0.8
     plot_w = len(ys) - 1 + W
@@ -31,12 +28,11 @@ def make_hist(ys, labels):
     for side in ('top', 'left', 'right'):
         ax.spines[side].set_visible(False)
     ax.yaxis.set_visible(False)
-    labels = [format_label(l) for l in labels]
     pyplot.xticks([x + W for x in xs], labels, rotation=45, ha='right')
     ax.xaxis.set_ticklabels([str(l) for l in labels])
     ax.tick_params(top=False, bottom=False)
 
-    pyplot.bar(xs, ys, width = 0.8, color = colors, edgecolor = edgecolors, linewidth=0.5)
+    pyplot.bar(xs, ys, width = W, color = colors, edgecolor = edgecolors, linewidth=0.5)
 
 batch, fname = filename("table-histograms.pdf")
 
@@ -45,7 +41,7 @@ if batch:
 
 YS = [[1,3,3],[2,1.01,2,2],[1,6],[1,6]]
 
-LABELS = [["1970","2014","2015"], ["Wed.","Thu.","Fri.","Sun."], [8, 12], [r"nnn", r"nn"]]
+LABELS = [["1970","2014","2015"], ["Wed.","Thu.","Fri.","Sun."], ["8", "12"], [r"nnn ", r"nn"]]
 
 for ys, labels in zip(YS, LABELS):
     make_hist(ys, labels)
